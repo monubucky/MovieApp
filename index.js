74acb43a4f974442c8c1270f005fdfcb31bdbcf0
unknown <monilnbakshi@gmail.com>
2023-12-15 19:38:20 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("om gan ganpataye namh....");
+const express = require('express');
+const app = express();
+const config = require('./config');
+app.get('/',(req,res)=>{
+    res.render('./config');
+});
+app.listen(5000, () => {
+    console.log("Success " + config.config.name);
+})
